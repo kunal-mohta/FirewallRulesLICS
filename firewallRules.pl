7907a3fabf2aa689fdc0bcfacd74c10fea460380
kunal-mohta <kunalmohta1818@gmail.com>
2018-11-26 19:23:43 +0530

    refactor

--- firewallRules.pl
+++ firewallRules.pl
@@ -9,81 +9,54 @@ member(X, [X|_]).
 member(X, [_|Z]) :- member(X, Z).
 
 
-getSubStr([], _, F, F, _).
-getSubStr([H|Main], [H|Ends], Fin, Acc, 0) :-
-  getSubStr(Main, [H|Ends], Fin, [H|Acc], 1), !.
+getSubList([], _, F, F, _).
+getSubList([H|Main], [H|Ends], Fin, Acc, 0) :-
+  getSubList(Main, [H|Ends], Fin, [H|Acc], 1), !.
 
-getSubStr([T|Main], Ends, Fin, Acc, 1) :-
+getSubList([T|Main], Ends, Fin, Acc, 1) :-
   last(T, Ends),
-  getSubStr(Main, Ends, Fin, [T|Acc], 0), !.
+  getSubList(Main, Ends, Fin, [T|Acc], 0), !.
 
-getSubStr([X|Main], Ends, Fin, Acc, 1) :-
-  getSubStr(Main, Ends, Fin, [X|Acc], 1), !.
+getSubList([X|Main], Ends, Fin, Acc, 1) :-
+  getSubList(Main, Ends, Fin, [X|Acc], 1), !.
 
-getSubStr([_|Main], Ends, Fin, Acc, 0) :-
-  getSubStr(Main, Ends, Fin, Acc, 0), !.
+getSubList([_|Main], Ends, Fin, Acc, 0) :-
+  getSubList(Main, Ends, Fin, Acc, 0), !.
 
 % ----------------- Rules ----------------- %
-% rule('accept adapter any').
+rule('accept adapter any').
 rule('accept adapter F-H').
-% rule('reject adapter B').
+rule('reject adapter B').
 rule('reject adapter D-F').
-% rule('drop adapter C').
+rule('drop adapter C').
 rule('drop adapter D,F').
 rule('drop adapter A-C').
 
 % ----------------- Parsing Rules ----------------- %
 
-adapter(IdList, accept) :-
+adapter(IdList, ResponseTerm) :-
   rule(Rule),
-  split_string(Rule, ' ', '', ["accept"|SplitText]),
+  split_string(Rule, ' ', '', [ResponseString|SplitText]),
+  term_string(ResponseTerm, ResponseString),
+  last(Ids, SplitText),
+  split_string(Ids, ',', '', IdList).
+
+adapter(IdList, ResponseTerm) :-
+  rule(Rule),
+  split_string(Rule, ' ', '', [ResponseString|SplitText]),
+  term_string(ResponseTerm, ResponseString),
   last(Ids, SplitText),
   string_chars(Ids, IdChars),
   member(',', IdChars),
   split_string(Ids, ',', '', IdList).
 
-adapter(IdList, reject) :-
+adapter(IdList, ResponseTerm) :-
   rule(Rule),
-  split_string(Rule, ' ', '', ["reject"|SplitText]),
-  last(Ids, SplitText),
-  string_chars(Ids, IdChars),
-  member(',', IdChars),
-  split_string(Ids, ',', '', IdList).
-
-adapter(IdList, drop) :-
-  rule(Rule),
-  split_string(Rule, ' ', '', ["drop"|SplitText]),
-  last(Ids, SplitText),
-  string_chars(Ids, IdChars),
-  member(',', IdChars),
-  split_string(Ids, ',', '', IdList).
-
-adapter(IdList, accept) :-
-  rule(Rule),
-  split_string(Rule, ' ', '', ["accept"|SplitText]),
+  split_string(Rule, ' ', '', [ResponseString|SplitText]),
+  term_string(ResponseTerm, ResponseString),
   last(Ids, SplitText),
   string_chars(Ids, IdChars),
   member('-', IdChars),
   split_string(Ids, '-', '', IdEnds),
   mainStr(MainStr),
-  getSubStr(MainStr, IdEnds, IdList, [], 0).
-
-adapter(IdList, reject) :-
-  rule(Rule),
-  split_string(Rule, ' ', '', ["reject"|SplitText]),
-  last(Ids, SplitText),
-  string_chars(Ids, IdChars),
-  member('-', IdChars),
-  split_string(Ids, '-', '', IdEnds),
-  mainStr(MainStr),
-  getSubStr(MainStr, IdEnds, IdList, [], 0).
-
-adapter(IdList, drop) :-
-  rule(Rule),
-  split_string(Rule, ' ', '', ["drop"|SplitText]),
-  last(Ids, SplitText),
-  string_chars(Ids, IdChars),
-  member('-', IdChars),
-  split_string(Ids, '-', '', IdEnds),
-  mainStr(MainStr),
-  getSubStr(MainStr, IdEnds, IdList, [], 0).
+  getSubList(MainStr, IdEnds, IdList, [], 0).
