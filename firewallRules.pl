4757c0387d54a936c87c3c4bae8ac562904359f5
kunal-mohta <kunalmohta1818@gmail.com>
2018-11-26 18:50:07 +0530

    hyphenation - adapter clause

--- firewallRules.pl
+++ firewallRules.pl
@@ -1,4 +1,5 @@
 % ----------------- ADAPTER CLAUSE ----------------- %
+mainStr(["A", "B", "C", "D", "E", "F", "G", "H"]).
 
 % ----------------- Utilities ----------------- %
 last(X,[X]).
@@ -7,14 +8,29 @@ last(X,[_|Z]) :- last(X,Z).
 member(X, [X|_]).
 member(X, [_|Z]) :- member(X, Z).
 
+
+getSubStr([], _, F, F, _).
+getSubStr([H|Main], [H|Ends], Fin, Acc, 0) :-
+  getSubStr(Main, [H|Ends], Fin, [H|Acc], 1), !.
+
+getSubStr([T|Main], Ends, Fin, Acc, 1) :-
+  last(T, Ends),
+  getSubStr(Main, Ends, Fin, [T|Acc], 0), !.
+
+getSubStr([X|Main], Ends, Fin, Acc, 1) :-
+  getSubStr(Main, Ends, Fin, [X|Acc], 1), !.
+
+getSubStr([_|Main], Ends, Fin, Acc, 0) :-
+  getSubStr(Main, Ends, Fin, Acc, 0), !.
+
 % ----------------- Rules ----------------- %
 % rule('accept adapter any').
-rule('accept adapter A,H').
+rule('accept adapter F-H').
 % rule('reject adapter B').
-% rule('reject adapter E,G').
+rule('reject adapter D-F').
 % rule('drop adapter C').
 rule('drop adapter D,F').
-rule('drop adapter A-H').
+rule('drop adapter A-C').
 
 % ----------------- Parsing Rules ----------------- %
 
@@ -42,26 +58,32 @@ adapter(IdList, drop) :-
   member(',', IdChars),
   split_string(Ids, ',', '', IdList).
 
-adapter(IdEnds, accept) :-
+adapter(IdList, accept) :-
   rule(Rule),
   split_string(Rule, ' ', '', ["accept"|SplitText]),
   last(Ids, SplitText),
   string_chars(Ids, IdChars),
   member('-', IdChars),
-  split_string(Ids, '-', '', IdEnds).
+  split_string(Ids, '-', '', IdEnds),
+  mainStr(MainStr),
+  getSubStr(MainStr, IdEnds, IdList, [], 0).
 
-adapter(IdEnds, reject) :-
+adapter(IdList, reject) :-
   rule(Rule),
   split_string(Rule, ' ', '', ["reject"|SplitText]),
   last(Ids, SplitText),
   string_chars(Ids, IdChars),
   member('-', IdChars),
-  split_string(Ids, '-', '', IdEnds).
+  split_string(Ids, '-', '', IdEnds),
+  mainStr(MainStr),
+  getSubStr(MainStr, IdEnds, IdList, [], 0).
 
-adapter(IdEnds, drop) :-
+adapter(IdList, drop) :-
   rule(Rule),
   split_string(Rule, ' ', '', ["drop"|SplitText]),
   last(Ids, SplitText),
   string_chars(Ids, IdChars),
   member('-', IdChars),
-  split_string(Ids, '-', '', IdEnds).
+  split_string(Ids, '-', '', IdEnds),
+  mainStr(MainStr),
+  getSubStr(MainStr, IdEnds, IdList, [], 0).
