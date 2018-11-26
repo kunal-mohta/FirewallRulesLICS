9300aa9f4379e20ed049d75c15929d4bf06ea8b0
kunal-mohta <kunalmohta1818@gmail.com>
2018-11-26 16:17:58 +0530

    hyphenated adapter list start

--- firewallRules.pl
+++ firewallRules.pl
@@ -9,29 +9,59 @@ member(X, [_|Z]) :- member(X, Z).
 
 % ----------------- Rules ----------------- %
 % rule('accept adapter any').
-% rule('accept adapter A,H').
+rule('accept adapter A,H').
 % rule('reject adapter B').
 % rule('reject adapter E,G').
 % rule('drop adapter C').
-% rule('drop adapter D,F').
-rule('accept adapter A-H').
+rule('drop adapter D,F').
+rule('drop adapter A-H').
 
 % ----------------- Parsing Rules ----------------- %
 
 adapter(IdList, accept) :-
   rule(Rule),
-  split_string(Rule, " ", "", ["accept"|SplitText]),
+  split_string(Rule, ' ', '', ["accept"|SplitText]),
   last(Ids, SplitText),
-  split_string(Ids, ",", "", IdList).
+  string_chars(Ids, IdChars),
+  member(',', IdChars),
+  split_string(Ids, ',', '', IdList).
 
 adapter(IdList, reject) :-
   rule(Rule),
-  split_string(Rule, " ", "", ["reject"|SplitText]),
+  split_string(Rule, ' ', '', ["reject"|SplitText]),
   last(Ids, SplitText),
-  split_string(Ids, ",", "", IdList).
+  string_chars(Ids, IdChars),
+  member(',', IdChars),
+  split_string(Ids, ',', '', IdList).
 
 adapter(IdList, drop) :-
   rule(Rule),
-  split_string(Rule, " ", "", ["drop"|SplitText]),
+  split_string(Rule, ' ', '', ["drop"|SplitText]),
   last(Ids, SplitText),
-  split_string(Ids, ",", "", IdList).
+  string_chars(Ids, IdChars),
+  member(',', IdChars),
+  split_string(Ids, ',', '', IdList).
+
+adapter(IdEnds, accept) :-
+  rule(Rule),
+  split_string(Rule, ' ', '', ["accept"|SplitText]),
+  last(Ids, SplitText),
+  string_chars(Ids, IdChars),
+  member('-', IdChars),
+  split_string(Ids, '-', '', IdEnds).
+
+adapter(IdEnds, reject) :-
+  rule(Rule),
+  split_string(Rule, ' ', '', ["reject"|SplitText]),
+  last(Ids, SplitText),
+  string_chars(Ids, IdChars),
+  member('-', IdChars),
+  split_string(Ids, '-', '', IdEnds).
+
+adapter(IdEnds, drop) :-
+  rule(Rule),
+  split_string(Rule, ' ', '', ["drop"|SplitText]),
+  last(Ids, SplitText),
+  string_chars(Ids, IdChars),
+  member('-', IdChars),
+  split_string(Ids, '-', '', IdEnds).
