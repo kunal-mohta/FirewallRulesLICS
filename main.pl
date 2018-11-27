e9e8a6968887fb092c4b2681412e4a6081f7e610
kunal-mohta <kunalmohta1818@gmail.com>
2018-11-27 13:17:23 +0530

    tcp - single, comma, range

--- main.pl
+++ main.pl
@@ -1,16 +1,23 @@
-packet(AdapterId, EtherProtoId, EtherVId, X) :-
-  adapter(AdapterIds, X),
-  member(AdapterId, AdapterIds),
-  etherProto(X).
-
-packet(EtherProtoId, X) :-
-  etherNet(EtherProtoIds, X),
-  member(EtherProtoId, EtherProtoIds).
+% packet(AdapterId, EtherProtoId, EtherVId, X) :-
+%   adapter(AdapterIds, X),
+%   member(AdapterId, AdapterIds),
+%   etherProto(X).
 
 % packet(EtherProtoId, X) :-
-  % etherNet(EtherProtoId, X).
+%   etherNet(EtherProtoIds, X),
+%   member(EtherProtoId, EtherProtoIds).
 
-packet(EtherProtoId, EtherVId, X) :-
-  etherNet(EtherProtoIds, EtherVIds, X),
-  member(EtherProtoId, EtherProtoIds),
-  member(EtherVId, EtherVIds).
+% packet(EtherProtoId, X) :-
+%   etherNet(EtherProtoRangeStart, EtherProtoRangeStop, X),
+%   number_string(NumProtoId, EtherProtoId),
+%   NumProtoId >= EtherProtoRangeStart,
+%   NumProtoId =< EtherProtoRangeStop.
+
+% packet(EtherProtoId, EtherVId, X) :-
+%   etherNet(EtherProtoIds, EtherVIds, X),
+%   member(EtherProtoId, EtherProtoIds),
+%   member(EtherVId, EtherVIds).
+
+% packet(TCPsrcport, X) :- tcpSrc(TCPsrcport, X).
+% packet(TCPdstport, X) :- tcpDst(TCPdstport, X).
+packet(TCPsrcport, TCPdstport, X) :- tcpSrcDst(TCPsrcport, TCPdstport, X).
